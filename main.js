a6ddf9d5eb6448643bfbdb7417db4704f8a75685
frostie <mattwiner04@gmail.com>
2026-06-23 21:27:45 -0700

    Take it out to pomona

---
+++ main.js
@@ -0,0 +1,43 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+import multer from "multer";
+import dotenv from "dotenv";
+import { engine } from "express-handlebars";
+import session from 'express-session';
+import { fileURLToPath } from "url";
+import bcrypt from "bcrypt";
+dotenv.config();
+console.log(process.env.NODE_ENV);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+const VIEWS_DIR = path.join(__dirname, "views");
+const PARTIALS_DIR = path.join(VIEWS_DIR, "partials");
+const PUBLIC_DIR = path.join(__dirname, "public");
+const upload = multer();
+const PORT = process.env.PORT || 3003;
+
+// Template engine
+app.engine("html", engine({ extname: ".html", defaultLayout: false, partialsDir: PARTIALS_DIR }));
+app.set("view engine", "html");
+app.set("views", VIEWS_DIR);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(PUBLIC_DIR));
+
+// Routes
+app.get("/", (req, res) => {
+    res.json({"hello":"FGTEEVEE"});
+});
+app.get("/api/systems", (req, res) => {
+    res.json({"hello":"FGTEEVEE"});
+});
+// Start server
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
